2439c463f42dd072e91a613c296727389fb538a9
Kenny Ngo <kngo@umich.edu>
2022-04-27 10:19:33 -0400

    Updates

---
+++ precache-manifest.386440ab020c0e7df32a5298a07080e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75de0982072deae17276",
+    "url": "/kennyhngo/game-of-life/static/css/main.89ac6c0f.chunk.css"
+  },
+  {
+    "revision": "75de0982072deae17276",
+    "url": "/kennyhngo/game-of-life/static/js/main.75de0982.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/kennyhngo/game-of-life/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "97980bd0d7afa0b0f0fb",
+    "url": "/kennyhngo/game-of-life/static/js/2.97980bd0.chunk.js"
+  },
+  {
+    "revision": "efd3089cb35316d8f22d",
+    "url": "/kennyhngo/game-of-life/static/js/runtime~main.efd3089c.js"
+  },
+  {
+    "revision": "fb6ac664a19c84169768cca53bba29be",
+    "url": "/kennyhngo/game-of-life/index.html"
+  }
+];
