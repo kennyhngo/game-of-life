b3a16dc98e695e5637c89d153adf99a8066c22c5
Kenny Ngo <kngo@umich.edu>
2022-11-25 01:08:21 -0500

    Initial build subtree commit

---
+++ build/precache-manifest.1b7b4a5b43b7d155f58a6826cf23acb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a0719458eb8591fd6ea",
+    "url": "/kennyhngo/game-of-life/static/css/main.f1f47771.chunk.css"
+  },
+  {
+    "revision": "2a0719458eb8591fd6ea",
+    "url": "/kennyhngo/game-of-life/static/js/main.2a071945.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/kennyhngo/game-of-life/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "97980bd0d7afa0b0f0fb",
+    "url": "/kennyhngo/game-of-life/static/js/2.97980bd0.chunk.js"
+  },
+  {
+    "revision": "efd3089cb35316d8f22d",
+    "url": "/kennyhngo/game-of-life/static/js/runtime~main.efd3089c.js"
+  },
+  {
+    "revision": "1a5d7bb3fde20585de2d27ab536d2e5b",
+    "url": "/kennyhngo/game-of-life/index.html"
+  }
+];
